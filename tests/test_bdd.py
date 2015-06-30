43ccd19e6f6d4cb05167ffaf4bc1e15225f1befb
tlake <tanner.lake@gmail.com>
2015-06-30 11:27:17 -0700

    more restructuring

--- tests/test_bdd.py
+++ tests/test_bdd.py
@@ -4,3 +4,16 @@ from pytest_bdd import scenario, given, when, then
 @scenario('features/homepage.feature', 'The homepage lists entries for anon')
 def test_home_listing_as_anon():
     pass
+
+    # FEATURES
+
+
+@given('an anonymous user')
+def an_anonymous_user(app):
+    return app
+
+
+    # Given an anonymous user
+    # And a list of three entries
+    # When the user visits the homepage
+    # Then they see a list of three entries
